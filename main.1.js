de0de33e5b9622ba5ea38115171307fbf1914494
Sarthak Pokharel <54739445+SarthakWhenever@users.noreply.github.com>
2020-01-09 11:30:20 +0545

    def hajen

--- main.1.js
+++ main.1.js
@@ -1,18 +1,21 @@
 function proxy_request(url,opt,callback,error,def_heads){
     let proxy="https://req-pro.herokuapp.com/";
-    if (!url)throw new Error ("Invalid URL");
+    if (!url && !opt.url)throw new Error ("Invalid URL");
+    if(typeof opt == "function"){
+        callback = opt; opt=undefined;
+    };
     opt||(opt = {});
     opt.headers||(opt.headers= {});
     opt.url||(opt.url=url);
     if (def_heads!=false){
         opt.headers["User-agent"] = navigator&&navigator.userAgent;
     }
+    alert(opt.url)
     _request(proxy,{
-        url:opt.url,
         method:"POST",
         body:JSON.stringify({
             ...opt,
-            url:url
+            url:opt.url
         })
     },function (){
         callback(JSON.parse(this.response))
