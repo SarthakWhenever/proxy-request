7f3f540ebad1e65ce445c702693a87fa589ae268
Sarthak Pokharel <54739445+SarthakWhenever@users.noreply.github.com>
2020-01-09 11:21:23 +0545

    default headrrs system

--- main.1.js
+++ main.1.js
@@ -1,8 +1,14 @@
-
-function proxy_request(url,opt,callback,error){
+function proxy_request(url,opt,callback,error,def_heads){
     let proxy="https://req-pro.herokuapp.com/";
+    if (!url)throw new Error ("Invalid URL");
+    opt||(opt = {});
+    opt.headers||(opt.headers= {});
+    opt.url||(opt.url=url);
+    if (def_heads!=false){
+        opt.headers["User-agent"] = navigator&&navigator.userAgent;
+    }
     _request(proxy,{
-        url:url,
+        url:opt.url,
         method:"POST",
         body:JSON.stringify({
             ...opt,
